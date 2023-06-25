e41553f564c15e102c171fab4481b8169f3d19cd
AndreMatioski02 <matioskiandre4@gmail.com>
2023-06-25 00:15:47 -0300

    AddProduct to Order

--- src/pages/add-product-to-cart/index.tsx
+++ src/pages/add-product-to-cart/index.tsx
@@ -12,24 +12,34 @@ import {
   Text8,
   alert, 
 } from '@telefonica/mistica'
-import styles from "./AddProductToCart.module.css";
+import styles from "./AddProductToOrder.module.css";
 import { useRouter } from "next/router";
 import { api } from "@/services/base";
-import { CartProductType, ProductType } from "@/types/cart";
-import { formatCartDate } from "@/utilities/formatOrderDate";
+import { OrderProductType, ProductType, OrderType } from "@/types/types";
+import { formatOrderDate } from "@/utilities/formatOrderDate";
 
-export default function AddProductToCart() {
+export default function AddProductToOrder() {
   const [availableProducts, setAvailableProducts] = React.useState<ProductType[]>([]);
   const [selectedProduct, setSelectedProduct] = React.useState("");
   const [newQuantity, setNewQuantity] = React.useState("");
-  const [cartProducts, setCartProducts] = React.useState<CartProductType[]>([]);
+  const [orderProducts, setOrderProducts] = React.useState<OrderProductType[]>([]);
+  const [order, setOrder] = React.useState<OrderType[]>([]);
   const router = useRouter();
 
   React.useEffect(() => {
+    handleGetOrder();
     handleGetProducts();
-    handleFetchCartProducts();
+    handleFetchOrderProducts();
   }, []);
 
+  const handleGetOrder = async () => {
+    const { orderId } = router.query;
+    try {
+      await api.get(`/get/order/${orderId}`).then(res => setOrder(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
   const handleGetProducts = async () => {
     try {
       await api.get(`/get/products`).then(res => setAvailableProducts(res.data));
@@ -38,36 +48,52 @@ export default function AddProductToCart() {
     }
   }
 
-  const handleFetchCartProducts = async () => {
-    const { cartId } = router.query;
-    await api.get(`/get/cart_product/${cartId}`).then(res => setCartProducts(res.data));
+  const handleFetchOrderProducts = async () => {
+    const { orderId } = router.query;
+    try {
+      await api.get(`/get/order_product/${orderId}`).then(res => setOrderProducts(res.data));
+    } catch(err) {
+      setOrderProducts([]);
+      console.log(err);
+    }
   }
 
-  const handleAddProductToCart = async () => {
-    const { cartId } = router.query;
+  const calculateTotalValue = () => {
+    handleFetchOrderProducts();
+
+    let total = 0;
+
+    const calculate = React.useCallback(() => {
+      return orderProducts.forEach(product => {
+        total += product.unit_price * product.quantity
+      })
+    }, [orderProducts])
+
+    return calculate;
+  }
+
+  const handleAddProductToOrder = async () => {
+    const { orderId } = router.query;
     const productToAdd = availableProducts.find(availableProduct => availableProduct.id.toString() == selectedProduct);
 
     if(productToAdd) {
-      if(cartProducts.length > 0) {
-        const existingProductIndex = cartProducts.findIndex((cartProduct: CartProductType) => cartProduct.product_id.toString() === selectedProduct);
-        const myCart = await api.get(`/get/shopping_cart/${cartId}`).then(res => res.data[0]);
+      if(orderProducts.length > 0) {
+        const existingProductIndex = orderProducts.findIndex((orderProduct: OrderProductType) => orderProduct.product_id.toString() === selectedProduct);
+        const myOrder = await api.get(`/get/order/${orderId}`).then(res => res.data[0]);
 
         if (existingProductIndex == -1) {
           try{
-            await api.post("/create/cart_product", {
-              shopping_cart_id: cartId,
+            await api.post("/create/order_product", {
+              order_id: orderId,
               product_id: Number(selectedProduct),
               quantity: Number(newQuantity),
-              created_at: formatCartDate(new Date()),
-              total_value: (productToAdd.price*Number(newQuantity)).toFixed(2),
-              product_value: productToAdd.price.toFixed(2)
+              unit_price: productToAdd.price,
             });
-            await api.put(`/update/shopping_cart/${cartId}`, {
-              total_value: (myCart.total_value+(productToAdd.price*Number(newQuantity))).toFixed(2),
-              status: "P",
-              created_at: formatCartDate(new Date(myCart.created_at)),
-              updated_at: formatCartDate(new Date()),
-              user_id: myCart.user_id
+            await api.put(`/update/order/${orderId}`, {
+              client_id: myOrder.client_id,
+              created_at: formatOrderDate(new Date()),
+              updated_at: formatOrderDate(new Date()),
+              total_value: (productToAdd.price*Number(newQuantity))+Number(order[0].total_value),
             });
             alert({
               title: "Produto adicionado com sucesso!",
@@ -79,20 +105,19 @@ export default function AddProductToCart() {
             console.log(err);
           }
         }else {
-          const toUpdateProduct = cartProducts[existingProductIndex];
+          const toUpdateProduct = orderProducts[existingProductIndex];
           try{
-            await api.put(`/update/cart_product/${cartId}/${toUpdateProduct.product_id}`, {
-              quantity: (toUpdateProduct.quantity+Number(newQuantity)),
-              created_at: formatCartDate(new Date(toUpdateProduct.created_at)),
-              total_value: (toUpdateProduct.product_value*(toUpdateProduct.quantity+Number(newQuantity))).toFixed(2),
-              product_value: toUpdateProduct.product_value.toFixed(2)
+            await api.put(`/update/order_product/${orderId}/${toUpdateProduct.product_id}`, {
+              order_id: orderId,
+              product_id: Number(selectedProduct),
+              quantity: Number(newQuantity),
+              unit_price: toUpdateProduct.unit_price,
             });
-            await api.put(`/update/shopping_cart/${cartId}`, {
-              total_value: (myCart.total_value+(toUpdateProduct.product_value*Number(newQuantity))).toFixed(2),
-              status: "P",
-              created_at: formatCartDate(new Date(myCart.created_at)),
-              updated_at: formatCartDate(new Date()),
-              user_id: myCart.user_id
+            await api.put(`/update/order/${orderId}`, {
+              client_id: myOrder.client_id,
+              created_at: formatOrderDate(new Date()),
+              updated_at: formatOrderDate(new Date()),
+              total_value: (myOrder.total_value+(toUpdateProduct.unit_price*(Number(newQuantity)-toUpdateProduct.quantity))),
             });
             alert({
               title: "Produto adicionado com sucesso!",
@@ -117,7 +142,7 @@ export default function AddProductToCart() {
        </Box>
        <Box paddingTop={64}>
         <Select 
-          name="cart-product-addition"
+          name="order-product-addition"
           label="Escolha um..."
           options={availableProducts.map(availableProduct => {
             return {
@@ -141,7 +166,7 @@ export default function AddProductToCart() {
           <ButtonSecondary onPress={router.back}>
             Voltar
           </ButtonSecondary>
-          <ButtonPrimary disabled={!selectedProduct || !newQuantity} onPress={() => { handleAddProductToCart() }}>
+          <ButtonPrimary disabled={!selectedProduct || !newQuantity} onPress={() => { handleAddProductToOrder() }}>
             Adicionar
           </ButtonPrimary>
         </Inline>
